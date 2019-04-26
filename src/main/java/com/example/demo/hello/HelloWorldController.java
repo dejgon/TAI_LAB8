839c5b68d9e7132b67dde39c8f6eddf02836a2d2
dejgon <cop.damian@gmail.com>
2019-04-26 16:06:24 +0200

    Add functionality

---
+++ src/main/java/com/example/demo/hello/HelloWorldController.java
@@ -0,0 +1,22 @@
+package com.example.demo.hello;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping(path = "/hello-world")
+    public String helloWorld(){
+        return "Hello world!";
+    }
+
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("Hello world!");
+    }
+
+    @GetMapping(path = "hello-world/path-variable/{name}")
+    public HelloWorldBean helloWorldBeanPathVariable(@PathVariable String name) {
+        return new HelloWorldBean((String.format("Hello world %s", name)));
+    }
+}
